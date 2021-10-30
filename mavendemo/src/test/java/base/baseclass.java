dc2259d1b94539fc8f8a78f2793e86d0ce92d93d
BOSU <BOSU@DESKTOP-PJJDTV0>
2021-10-30 18:02:50 +0530

    first commit

---
+++ mavendemo/src/test/java/base/baseclass.java
@@ -0,0 +1,17 @@
+package base;
+
+import org.testng.annotations.Test;
+
+public class baseclass {
+	
+	@Test
+	public void test() {
+		System.out.println(".............welcome............");
+		System.out.println(".............welcome............");
+		System.out.println(".............welcome............");
+		System.out.println(".............welcome............");
+		System.out.println(".............welcome............");
+		
+	}
+
+}
